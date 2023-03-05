8b34acf3fd8b495b66b322e41681698933ccb854
Ivan <vanuk@bk.ru>
2023-03-05 19:16:31 +0800

    created example 5

---
+++ Example005/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Please enter your name: ");
+string userName = Console.ReadLine();
+if (userName == "masha")
+{
+    Console.WriteLine("Hurray!");
+}
+else
+{
+    Console.WriteLine("Hello, " + userName);
+}
